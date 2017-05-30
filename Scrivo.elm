99841cce4e6f8cad79111380b6475e7b42191d53
Eleni Lixourioti <contact@eleni.co>
2017-05-30 20:21:00 +0100

    Elm-ify lobby view

--- Scrivo.elm
+++ Scrivo.elm
@@ -2,7 +2,19 @@ module Scrivo exposing (..)
 
 import Html exposing (..)
 import Debug
-import Html.Attributes exposing (class, href, attribute)
+import Html.Attributes
+    exposing
+        ( class
+        , href
+        , attribute
+        , type_
+        , maxlength
+        , id
+        , hidden
+        , title
+        , placeholder
+        , name
+        )
 import Html.Events exposing (onClick)
 import Navigation exposing (Location)
 
@@ -72,20 +84,122 @@ viewInitial =
         ]
 
 
+viewNavigation : Html Msg
+viewNavigation =
+    nav
+        [ class "navbar navbar-default navbar-fixed-top" ]
+        [ div
+            [ class "container-fluid" ]
+            [ div
+                [ class "navbar-header" ]
+                [ a
+                    [ class "navbar-brand", href "#" ]
+                    [ text "Scrivo"
+                    , sup [] [ text "beta" ]
+                    ]
+                ]
+            ]
+        ]
+
+
 viewGameLobby : Html Msg
 viewGameLobby =
-    div [ class "container-fluid vertical-center" ] []
+    div
+        [ class "container-fluid main-content" ]
+        [ div
+            [ class "row flex-row" ]
+            [ div
+                [ class "col-md-3 col-md-offset-3" ]
+                [ h2 [] [ text "Game Code" ]
+                , div
+                    [ class "game-code round-colored-container" ]
+                    [ text "BJKWPTQ" ]
+                , div
+                    []
+                    [ h2
+                        []
+                        [ text "Profile" ]
+                    , form
+                        [ class "form round-container" ]
+                        [ div
+                            [ class "form-group" ]
+                            [ input
+                                [ type_ "text", class "form-control input-lg", maxlength 12, id "nickname", name "nickname", placeholder "Your name" ]
+                                []
+                            , button
+                                [ class "btn btn-lg btn-success btn-group-justified" ]
+                                [ text "Ready ?" ]
+                            ]
+                        ]
+                    ]
+                ]
+            , div
+                [ class "col-md-3" ]
+                [ h2
+                    []
+                    [ text "Players Joined" ]
+                , ul
+                    [ class "players-list round-colored-container" ]
+                    [ li
+                        []
+                        [ span
+                            [ class "glyphicon glyphicon-ok", title "Ready!" ]
+                            []
+                        , span
+                            [ class "nickname" ]
+                            [ text "Alpaca15234" ]
+                        ]
+                    , li
+                        []
+                        [ span
+                            [ class "glyphicon glyphicon-time", title "Not ready" ]
+                            []
+                        , span
+                            [ class "nickname" ]
+                            [ text "Unicorn6273" ]
+                        ]
+                    , li
+                        []
+                        [ span
+                            [ class "glyphicon glyphicon-ok", title "Ready!" ]
+                            []
+                        , span
+                            [ class "nickname" ]
+                            [ text "Hippo65228" ]
+                        ]
+                    , li
+                        []
+                        [ span
+                            [ class "glyphicon glyphicon-pencil", title "Free spot" ]
+                            []
+                        , span
+                            [ class "nickname" ]
+                            [ text "-- Free spot --" ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+
+
+appView : Html Msg -> Html Msg
+appView msg =
+    -- Add navigation to main app views
+    div [] [ viewNavigation, msg ]
 
 
 view : Model -> Html Msg
 view model =
     case model.gameState of
         CreatingGame ->
-            viewGameLobby
+            appView viewGameLobby
 
         JoiningGame ->
-            div [ class "container-fluid vertical-center" ]
-                [ h1 [] [ text "Join Game" ] ]
+            appView
+                (div
+                    [ class "container-fluid vertical-center" ]
+                    [ h1 [] [ text "Join Game" ] ]
+                )
 
         _ ->
             viewInitial
